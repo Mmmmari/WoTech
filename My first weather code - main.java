3989eb929def90788cd700eb6a051c5669485b9d
Mmmmari <165931583+Mmmmari@users.noreply.github.com>
2024-04-03 19:19:26 +0300

    Create My first weather code - main.java

---
+++ My first weather code - main.java
@@ -0,0 +1,53 @@
+public class Main {
+  public static void main(String[] args) {
+
+    //winter, spring, summer, autumn
+    //warm jacket, t-shirt, swimwear, rain coat
+
+    String season = "Summer";
+    
+    if (season == "Winter"){
+      System.out.println("Wear a warm jacket");
+      }
+    else if (season == "Spring"){
+      System.out.println("Wear a T-shirt");
+      }
+    else if (season == "Summer"){
+      System.out.println("Wear swimwear");
+      }
+    else if (season == "Autumn"){
+      System.out.println("Wear a rain coat");
+    }
+    else {
+      System.out.println("Invalid season");
+    }
+
+   
+    double temperature = 20.0;
+
+    if (temperature <= 5) {
+        System.out.println("Wear super warm clothes.");
+    } else if (temperature <= 15) {
+        System.out.println("Wear warm clothes.");
+    } else if (temperature <= 30) {
+        System.out.println("Wear normal clothes.");
+    } else {
+        System.out.println("You need cooling.");
+    }
+
+
+    double temperatures = -15.0; 
+    if (temperatures <= 5) {
+      System.out.println("Wear super warm clothes.");
+    } else if (temperatures <= 15) {
+      System.out.println("Wear warm clothes."); // if I would put only "if" here, it would print "Wear warm clothes." and "You need cooling."
+    } else if (temperatures <= 30) {
+      System.out.println("Wear normal clothes.");
+    } else {
+      System.out.println("You need cooling.");
+    }
+
+
+  }
+
+}
